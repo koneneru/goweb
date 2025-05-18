a9e0e9535fbca4f8e363f23d265615b2af59fc27
k0neru <koneneru@gmail.com>
2025-05-18 23:29:08 +0400

    Encapsulating the API routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("POST /v1/books", app.createBookHandler)
+	mux.HandleFunc("GET /v1/books/{id}", app.showBookHandler)
+
+	return mux
+}
