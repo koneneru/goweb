c6bcb957ea2ca7a0439119d5afda4144ec16fa98
k0neru <koneneru@gmail.com>
2025-05-18 23:25:24 +0400

    Added showBookHandler

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new book")
+}
+
+func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) {
+	param := r.PathValue("id")
+
+	id, err := strconv.ParseInt(param, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of book %d\n", id)
 }
