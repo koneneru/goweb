b2cfcc7a9bd7229cd3e87a5df92153859868f1d6
k0neru <koneneru@gmail.com>
2025-05-21 23:10:15 +0400

    Added helper for extracting ID param from URL

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	param := r.PathValue("id")
+
+	id, err := strconv.ParseInt(param, 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
