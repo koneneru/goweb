7c2cc722809993ad78b95cffc632f5e4f86accaa
k0neru <koneneru@gmail.com>
2025-05-22 12:05:29 +0400

    Change keys in JSON of Book struct

--- internal/data/book.go
+++ internal/data/book.go
@@ -3,12 +3,12 @@ package data
 import "time"
 
 type Book struct {
-	ID        int64
-	CreatedAt time.Time
-	Title     string
-	Author    string
-	Year      int32
-	Size      int32
-	Genres    []string
-	Version   int32
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	Year      int32     `json:"year"`
+	Size      int32     `json:"size"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"`
 }
