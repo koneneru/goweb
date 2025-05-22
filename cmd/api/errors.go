3ede134c52b88048784f54abf813d72a7c078cd7
k0neru <koneneru@gmail.com>
2025-05-22 15:40:17 +0400

    Added func to send error response

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,3 +5,13 @@ import "net/http"
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
 }
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	envelope := envelope{"error": message}
+
+	err := app.writeJSON(w, status, envelope, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
