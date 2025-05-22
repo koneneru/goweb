1f47678c1fdbb77d25ac308d81d6fa0abf663f8c
k0neru <koneneru@gmail.com>
2025-05-22 11:55:38 +0400

    Updated showBookHandler() to send dummy data as a JSON response using helpers.writeJSON()

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"goweb/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +18,19 @@ func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of book %d\n", id)
+	book := data.Book{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Spice and Wolf vol.1",
+		Author:    "Isuna Hasekura",
+		Size:      289,
+		Genres:    []string{"adventure", "drama", "fantasy", "romance", "supernatural"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, book, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterred a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
