1930b7d8cc38d4de7c5aa20fbdf41efd5b21f516
k0neru <koneneru@gmail.com>
2025-05-22 11:40:14 +0400

    Defined custom Book struct

--- internal/data/book.go
+++ internal/data/book.go
@@ -1,1 +1,14 @@
 package data
+
+import "time"
+
+type Book struct {
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Author    string
+	Year      int32
+	Size      int32
+	Genres    []string
+	Version   int32
+}
