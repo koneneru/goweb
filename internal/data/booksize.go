ef822120c129e7351410d09a31a26c439c4fe2dc
k0neru <koneneru@gmail.com>
2025-05-23 08:58:51 +0400

    Added custom JSON decoding

--- internal/data/booksize.go
+++ internal/data/booksize.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidBooksizeFormat = errors.New("invalid booksize format")
 
 type Booksize int32
 
@@ -13,4 +17,25 @@ func (bs Booksize) MarshalJSON() ([]byte, error) {
 	quotedJsonValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJsonValue), nil
+}
+
+func (bs *Booksize) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJsonValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidBooksizeFormat
+	}
+
+	parts := strings.Split(unquotedJsonValue, " ")
+	if len(parts) != 2 || parts[1] != "pages" {
+		return ErrInvalidBooksizeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidBooksizeFormat
+	}
+
+	*bs = Booksize(i)
+
+	return nil
 }
