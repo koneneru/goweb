e76e21f0fa8b3e8f1cc768a82d33f51b114bb1dd
k0neru <koneneru@gmail.com>
2025-05-23 16:46:16 +0400

    Created migration to fill books table with mock data

---
+++ migrations/000003_fill_book_table.up.sql
@@ -0,0 +1,14 @@
+INSERT INTO books (title, author, year, size, genres)
+VALUES ('Spice and Wolf vol.1', 'Isuna Hasekura', 2006, 289, '{"adventure","drama","fantasy","romance","supernatural"}');
+
+INSERT INTO books (title, author, year, size, genres)
+VALUES ('Full Metal Panic! vol.1', 'Shouji Gatou', 1998, 343, '{"action","comedy","drama","romance","sci-fi"}');
+
+INSERT INTO books (title, author, year, size, genres)
+VALUES ('86: Eighty Six vol.1', 'Asato Asato', 2017, 324, '{"Action","drama","sci-fi"}');
+
+INSERT INTO books (title, author, year, size, genres)
+VALUES ('Mikkakan no Koufuku', 'Sugaru Miaki', 2013, 355, '{"drama"}');
+
+INSERT INTO books (title, author, year, size, genres)
+VALUES ('Kusuria no Hitorigoto vol.1', 'Natsu Huuga', 2014, 287, '{"drama","mystery"}');
