9f2e6be47f9c5be5bae6304b5642bf64fec54054
k0neru <koneneru@gmail.com>
2025-05-26 10:56:10 +0400

    Added route to list books

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,6 +7,7 @@ func (app *application) routes() *http.ServeMux {
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 
+	mux.HandleFunc("GET /v1/books", app.listBooksHandler)
 	mux.HandleFunc("POST /v1/books", app.createBookHandler)
 	mux.HandleFunc("GET /v1/books/{id}", app.showBookHandler)
 	mux.HandleFunc("PATCH /v1/books/{id}", app.updateBookHandler)
