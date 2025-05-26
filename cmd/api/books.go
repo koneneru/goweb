e127671c74875429cbd508338574b716d85de198
k0neru <koneneru@gmail.com>
2025-05-26 11:29:28 +0400

    Updated to set supported values in SortSafelist and validate filters

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -192,11 +192,14 @@ func (app *application) listBooksHandler(w http.ResponseWriter, r *http.Request)
 	input.Title = app.readString(qs, "title", "")
 	input.Author = app.readString(qs, "author", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
+
 	input.Filters.Page = app.readInt(qs, "page", 1, v)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
-	input.Filters.Sort = app.readString(qs, "sort", "id")
 
-	if !v.Valid() {
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafeList = []string{"id", "title", "author", "year", "-id", "-title", "-author", "-year"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
