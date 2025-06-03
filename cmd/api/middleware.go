ee6958fc354611c2d5fc7839b217db4a90bb23a2
k0neru <koneneru@gmail.com>
2025-06-03 16:08:01 +0400

    Updated to get real IP address for the client

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"goweb/internal/data"
 	"goweb/internal/validator"
-	"net"
 	"net/http"
 	"slices"
 	"strconv"
@@ -15,6 +14,7 @@ import (
 	"time"
 
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -57,11 +57,7 @@ func (app *application) rateLimit(netx http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
